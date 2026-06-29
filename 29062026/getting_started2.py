19078d513248e65087666b29970e0073b912cf8a
Huxley Tran <huxleytran@gmail.com>
2026-06-29 04:14:36 +0000

    add new practice 2 and 3 for 29062026

---
+++ 29062026/getting_started2.py
@@ -0,0 +1,11 @@
+# Kiểu dữ liệu List trong python
+
+# List trong python rất mạnh, nó giống array là có thể lưu trữ nhiều giá trị và đặc biệt nữa là các giá trị đó có thể khác nhau về kiểu dữ liệu (có thể là int, string, float, boolean,v.v..)
+
+skills = ["Python", "REST API", "n8n", "Chatbot"]
+skills.append(1)
+print(skills)
+# print(skills[0])
+# print(skills[1])
+print(skills[-1])
+# print(len(skills))
---
+++ 29062026/getting_started3.py
@@ -0,0 +1,18 @@
+# Kiểu dữ liệu Dictionary
+
+# Kiểu này giống y chang Object/JSON trong nodejs và Hashmap trong Java (chứa các object gồm key và value)
+
+freelancer = {
+    "name": "Huxley",
+    "age": 23,
+    "goal_salary": 5000,
+    "skill_set": ["Python", "REST API", "Chatbot", "Framework", "AI using"]
+}
+
+# print(freelancer["name"])
+# print(freelancer["age"])
+# print(freelancer["goal_salary"])
+
+freelancer["gender"] = "Nam"
+freelancer["is_a_good_dev"] = True
+print(freelancer)
